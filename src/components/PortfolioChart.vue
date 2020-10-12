546883cb98ce228ac0715b4ec05cdcdbd77299e2
Sergio Schiavello <sergio@Sergios-MBP.lan>
2020-10-13 00:51:25 +0200

    Display chart with mock data

--- src/components/PortfolioChart.vue
+++ src/components/PortfolioChart.vue
@@ -1,15 +1,63 @@
 <template>
     <div id="portfolioChart">
-
+        <canvas id="pChartCanvas"/>
     </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
+
 export default {
     data() {
         return {
-
+            chartdata: [10,5,-10]
         }
+    },
+    mounted() {
+        var ctx = document.getElementById("pChartCanvas").getContext("2d");
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [1,2,3],
+                datasets: [{
+                    label: 'This week',
+                    cubicInterpolationMode: 'monotone',
+                    fill: true,
+                    data: this.chartdata,
+                    borderColor: '#003459',
+                    backgroundColor: 'rgba(0, 52, 89, .4)'
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            color: "transparent",
+                            display: true,
+                            drawBorder: false,
+                            zeroLineColor: "#ccc",
+                            zeroLineWidth: 1
+                        },
+                        ticks: {
+                            display: false
+                        },
+                        offset: true
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false
+                        },
+                        ticks: {
+                            display: false,
+                            padding: 100
+                        }
+                    }]
+                }
+            }
+        })
     }
 }
 </script>
@@ -25,5 +73,11 @@ export default {
         border: 1px solid rgba(0, 52, 89, 0.03);
         box-shadow: 4px 4px 10px rgba(0, 52, 89, 0.25);
         border-radius: 13px;
+    }
+
+    #pChartCanvas {
+        padding-top: 10px;
+        width: 710px !important;
+        margin-left: -10px;
     }
 </style>

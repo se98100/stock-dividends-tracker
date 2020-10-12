6ec7126d0353d03e178607468cc0cfc67123cc80
Sergio Schiavello <sergio@Sergios-MBP.lan>
2020-10-12 18:49:53 +0200

    Added site header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-
+  <site-header title="Dividends tracker" header-height="55px"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand">
 </template>
 
 <script>
+import SiteHeader from './components/SiteHeader'
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    SiteHeader
+  }
 }
 </script>
 
 <style>
-  
+  * {
+    font-family: Quicksand;
+    font-style: normal;
+  }
 </style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="header" :style="{height : headerHeight}">
+        <span id="title" :style="{lineHeight : headerHeight}">{{title}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            height : "75px"
+        }
+    },
+    props: ['title','headerHeight']
+}
+</script>
+
+<style scoped>
+    #header {
+        position: absolute;
+        width: 100%;
+        left: 0px;
+        top: 0px;
+
+        text-align: center;
+
+        background: #00A7E1;
+    }
+
+    #title {
+        font-weight: bold;
+        font-size: 36px;
+
+        color: #FFFFFF;
+    }
+</style>
